0618f994b56c083be8c50608347e6e0b1c7253c7
Volodymyr Hnatiuk <vololdymyr.gn@gmail.com>
2024-03-21 14:26:16 +0200

    Add GlobalExceptionHandler

---
+++ src/main/java/org/hnatiuk/carnotificator/advice/ErrorResponse.java
@@ -0,0 +1,16 @@
+package org.hnatiuk.carnotificator.advice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author Hnatiuk Volodymyr on 21.03.2024.
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+public class ErrorResponse {
+    private String message;
+    private Long timestamp;
+}
