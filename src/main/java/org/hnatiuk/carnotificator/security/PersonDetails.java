c88d6d48d06b031507a38ffb3733c109f982d224
Volodymyr Hnatiuk <vololdymyr.gn@gmail.com>
2024-03-21 12:08:05 +0200

    Add PersonDetails and PersonDetailsService for Spring Security

---
+++ src/main/java/org/hnatiuk/carnotificator/security/PersonDetails.java
@@ -0,0 +1,50 @@
+package org.hnatiuk.carnotificator.security;
+
+import org.hnatiuk.carnotificator.model.entity.Person;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * @author Hnatiuk Volodymyr on 21.03.2024.
+ */
+public record PersonDetails(Person person) implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(person.getRole()));
+    }
+
+    @Override
+    public String getPassword() {
+        return person.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return person.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
