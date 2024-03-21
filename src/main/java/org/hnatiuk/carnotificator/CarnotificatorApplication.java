4015980efbf57116610b1811038ec5317502512a
Volodymyr Hnatiuk <vololdymyr.gn@gmail.com>
2024-03-21 09:46:40 +0200

    Initial

---
+++ src/main/java/org/hnatiuk/carnotificator/CarnotificatorApplication.java
@@ -0,0 +1,13 @@
+package org.hnatiuk.carnotificator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarnotificatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarnotificatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hnatiuk/carnotificator/CarnotificatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hnatiuk.carnotificator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarnotificatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
