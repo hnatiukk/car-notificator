db32c63b5deef3a3dbc735a72f9c7d87bbab5712
Volodymyr Hnatiuk <vololdymyr.gn@gmail.com>
2024-03-22 17:37:44 +0200

    Enable csrf

--- UserService/src/main/java/ua/hnatiuk/userservice/config/SecurityConfig.java
+++ UserService/src/main/java/ua/hnatiuk/userservice/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
+                //.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(requests ->
                         requests.requestMatchers("/login", "/signup").permitAll()
                                 .anyRequest().authenticated())
