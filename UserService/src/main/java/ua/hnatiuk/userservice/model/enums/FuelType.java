d59b87ea4425034fd7bd40346e3c182aca99ec31
Volodymyr Hnatiuk <vololdymyr.gn@gmail.com>
2024-03-22 14:01:08 +0200

    Fill enums

--- UserService/src/main/java/ua/hnatiuk/userservice/model/enums/FuelType.java
+++ UserService/src/main/java/ua/hnatiuk/userservice/model/enums/FuelType.java
@@ -1,7 +1,21 @@
 package ua.hnatiuk.userservice.model.enums;
 
+import lombok.RequiredArgsConstructor;
+
 /**
  * @author Hnatiuk Volodymyr on 21.03.2024.
  */
+@RequiredArgsConstructor
 public enum FuelType {
+    GASOLINE("Бензин"),
+    DIESEL("Дизель"),
+    GAS("Газ"),
+    GAS_GASOLINE("Газ / бензин"),
+    HYBRID("Гібрид"),
+    ELECTRICAL("Електро");
+    private final String name;
+    @Override
+    public String toString() {
+        return name;
+    }
 }
--- UserService/src/main/java/ua/hnatiuk/userservice/model/enums/TransmissionType.java
+++ UserService/src/main/java/ua/hnatiuk/userservice/model/enums/TransmissionType.java
@@ -1,7 +1,21 @@
 package ua.hnatiuk.userservice.model.enums;
 
+import lombok.RequiredArgsConstructor;
+
 /**
  * @author Hnatiuk Volodymyr on 21.03.2024.
  */
+@RequiredArgsConstructor
 public enum TransmissionType {
+    MANUAL("Ручна / Механіка"),
+    AUTOMATIC("Автомат"),
+    TIPTRONIC("Типтронік"),
+    ROBOT("Робот"),
+    VARIATOR("Варіатор");
+    private final String name;
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
