0edb5a40124c4c566a225dd9410c2ff5b009b4ec
Volodymyr Hnatiuk <vololdymyr.gn@gmail.com>
2024-03-23 20:36:58 +0200

    Add validation of brand and model

--- UserService/src/main/java/ua/hnatiuk/userservice/util/SubscriptionValidator.java
+++ UserService/src/main/java/ua/hnatiuk/userservice/util/SubscriptionValidator.java
@@ -1,16 +1,20 @@
 package ua.hnatiuk.userservice.util;
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 import ua.hnatiuk.userservice.model.entity.Subscription;
+import ua.hnatiuk.userservice.service.JsonLoaderService;
 
 /**
  * @author Hnatiuk Volodymyr on 23.03.2024.
  */
 @Component
+@RequiredArgsConstructor
 public class SubscriptionValidator implements Validator {
+    private final JsonLoaderService jsonLoaderService;
     @Override
     public boolean supports(Class<?> clazz) {
         return Subscription.class.equals(clazz);
@@ -19,6 +23,12 @@ public class SubscriptionValidator implements Validator {
     @Override
     public void validate(Object target, Errors errors) {
         Subscription subscription = (Subscription) target;
+        if (!jsonLoaderService.getBrands().containsKey(subscription.getBrand())) {
+            errors.rejectValue("brand", "", "Такої марки не існує");
+        }
+        if (!jsonLoaderService.getModels().containsKey(subscription.getModel())) {
+            errors.rejectValue("model", "", "Такої моделі не існує");
+        }
         if (subscription.getPriceStart() != null && subscription.getPriceEnd() != null) {
             if (subscription.getPriceStart() >= subscription.getPriceEnd()) {
                 errors.rejectValue("priceStart", "", "Повинна бути менше");
