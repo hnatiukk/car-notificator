9b22c1b8766417ea033d0ed10ec4c4df2a9c5ef4
Volodymyr Hnatiuk <vololdymyr.gn@gmail.com>
2024-03-26 12:50:07 +0200

    Add new module observer-service

---
+++ observer-service/src/main/java/ua/hnatiuk/observerservice/ObserverServiceApplication.java
@@ -0,0 +1,13 @@
+package ua.hnatiuk.observerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObserverServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObserverServiceApplication.class, args);
+    }
+
+}
---
+++ observer-service/src/test/java/ua/hnatiuk/observerservice/ObserverServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.hnatiuk.observerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObserverServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
