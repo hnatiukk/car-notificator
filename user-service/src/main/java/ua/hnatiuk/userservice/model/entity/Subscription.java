631564c392edb21c4e3dbcacca6004b3f3baf703
Volodymyr Hnatiuk <vololdymyr.gn@gmail.com>
2024-03-28 08:33:13 +0200

    Remove unnecessary import

--- user-service/src/main/java/ua/hnatiuk/userservice/model/entity/Subscription.java
+++ user-service/src/main/java/ua/hnatiuk/userservice/model/entity/Subscription.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 import ua.hnatiuk.userservice.model.enums.FuelType;
 import ua.hnatiuk.userservice.model.enums.TransmissionType;
 
